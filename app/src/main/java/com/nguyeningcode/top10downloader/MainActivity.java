9ccc186f39fbf9d414913c2028841d12e0d8fe76
jnguyen7410 <jnguyen7410@gmail.com>
2017-04-29 18:16:57 -0700

    Small update  to sync laptop to desktop

--- app/src/main/java/com/nguyeningcode/top10downloader/MainActivity.java
+++ app/src/main/java/com/nguyeningcode/top10downloader/MainActivity.java
@@ -1,7 +1,8 @@
 package com.nguyeningcode.top10downloader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    // async type - String for input (url), void for progress (short, no progress bar needed), string for result
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
